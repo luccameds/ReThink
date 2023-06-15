0c937fa61f5135000e65a83b04d9fd6c17ba9145
luccameds <luccameds@gmail.com>
2023-06-15 18:35:31 -0300

    Created the Black and Green block in components

--- components/components2/GreenBlock.jsx
+++ components/components2/GreenBlock.jsx
@@ -1,8 +1,10 @@
+'use client';
+
 import React from 'react';
 
 function GreenBlock() {
   return (
-    <div className="absolute left-[900px] top-[-300px] z-[100px] flex h-[900px] w-[660.16px] rotate-[30deg] rounded-[100px] bg-gradient-to-r from-[#61DA17] to-[#D3F13A] opacity-10" />
+    <div className="absolute left-[900px] top-[-300px] z-[100px] flex h-[80%] w-1/2 rotate-[-45deg] rounded-[50px] bg-gradient-to-r from-[#61DA17] to-[#D3F13A] opacity-10" />
   );
 }
 
