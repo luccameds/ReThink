579d9235f8ec918c5021b0dd4d1f4a1115bde0bb
luccameds <luccameds@gmail.com>
2023-06-14 23:47:55 -0300

    Created the BlackBlock in components

---
+++ components/components2/BlackBlock.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BlackBlock() {
+  return (
+    <div className="absolute left-[100px] top-[600px] z-[100px] flex h-[900px] w-[660px] rotate-[30deg] rounded-[100px] bg-gradient-to-r from-[#1B1B1B] to-[#101010] opacity-30"></div>
+  );
+}
+
+export default BlackBlock;
