e9ad14bbe715ac2f92e142378cd0f5d5272c32ec
luccameds <luccameds@gmail.com>
2023-06-15 20:25:42 -0300

    Updated the size of the Black/Green blocks in components

--- components/components2/BlackBlock.jsx
+++ components/components2/BlackBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function BlackBlock() {
   return (
-    <div className="absolute left-[100px] top-[600px] z-[-100px] flex h-[80%] w-1/2 rotate-[-30deg] rounded-[100px] bg-gradient-to-r from-[#1B1B1B] to-[#101010] opacity-20"></div>
+    <div className="absolute left-[100px] top-[600px] z-[-100px] flex h-[900px] w-[900px] rotate-[-45deg] rounded-[100px] bg-gradient-to-r from-[#1B1B1B] to-[#101010] opacity-20"></div>
   );
 }
 
--- components/components2/GreenBlock.jsx
+++ components/components2/GreenBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function GreenBlock() {
   return (
-    <div className="absolute right-0 flex h-[50%] w-[50%] rotate-[45deg] rounded-[50px] bg-gradient-to-r from-[#61DA17] to-[#D3F13A] opacity-10" />
+    <div className="absolute right-0 flex h-[900px] w-[900px] rotate-[45deg] rounded-[50px] bg-gradient-to-r from-[#61DA17] to-[#D3F13A] opacity-10" />
   );
 }
 
