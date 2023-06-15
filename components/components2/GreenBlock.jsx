cc2dcc097b61fbf21a5182e92c1597742bf9892c
luccameds <luccameds@gmail.com>
2023-06-14 23:48:09 -0300

    Created the GreenBlock in components

---
+++ components/components2/GreenBlock.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function GreenBlock() {
+  return (
+    <div className="absolute left-[900px] top-[-300px] z-[100px] flex h-[900px] w-[660.16px] rotate-[30deg] rounded-[100px] bg-gradient-to-r from-[#61DA17] to-[#D3F13A] opacity-10" />
+  );
+}
+
+export default GreenBlock;
