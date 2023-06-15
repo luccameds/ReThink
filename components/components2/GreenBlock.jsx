160ae8320c65ef37cfc0472f150402c1138d16f9
luccameds <luccameds@gmail.com>
2023-06-15 19:32:18 -0300

    Upload the left position in GreenBlock in component

--- components/components2/GreenBlock.jsx
+++ components/components2/GreenBlock.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 function GreenBlock() {
   return (
-    <div className="absolute left-[900px] top-[-300px] z-[100px] flex h-[80%] w-1/2 rotate-[-45deg] rounded-[50px] bg-gradient-to-r from-[#61DA17] to-[#D3F13A] opacity-10" />
+    <div className="absolute left-[750px] top-[-300px] z-[100px] flex h-[80%] w-1/2 rotate-[-45deg] overflow-x-hidden rounded-[50px] bg-gradient-to-r from-[#61DA17] to-[#D3F13A] opacity-10" />
   );
 }
 
