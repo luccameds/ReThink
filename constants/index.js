185266a9639e7ea4bca0464ee482b376f0d14fc0
luccameds <luccameds@gmail.com>
2023-06-15 18:35:53 -0300

    Created the constants with the logo

---
+++ constants/index.js
@@ -0,0 +1,34 @@
+export const logos = [
+  {
+    id: 'logo-1',
+    imgUrl: '/american.png',
+  },
+  {
+    id: 'logo-2',
+    imgUrl: '/basf.png',
+  },
+  {
+    id: 'logo-3',
+    imgUrl: '/clarice.png',
+  },
+  {
+    id: 'logo-4',
+    imgUrl: '/dotz.png',
+  },
+  {
+    id: 'logo-5',
+    imgUrl: '/esfera.png',
+  },
+  {
+    id: 'logo-6',
+    imgUrl: '/fundep.png',
+  },
+  {
+    id: 'logo-7',
+    imgUrl: '/gasbrasiliano.png',
+  },
+  {
+    id: 'logo-8',
+    imgUrl: '/gol.png',
+  },
+];
