17bb68777df626aa66c1573ceb1cf235bccfc060
luccameds <luccameds@gmail.com>
2023-06-14 23:25:32 -0300

    first commit

---
+++ sections/Home.jsx
@@ -0,0 +1,13 @@
+import { Header, Hero } from '@/components';
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
